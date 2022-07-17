8b6fef569ff72ef3dc90914fe2f38500f01c2e6a
Christian <felix.christian.reyes@gmail.com>
2022-07-18 02:44:29 +0800

    added Services component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import Hero from './pages/Home/Hero/Hero';
 import AboutUs from './pages/Home/AboutUs/AboutUs';
+import Services from './pages/Home/Services/Services';
 
 
 
@@ -14,6 +15,7 @@ function App() {
       <Navbar />
       <Hero />
       <AboutUs />
+      <Services />
     </div>
   );
 }
