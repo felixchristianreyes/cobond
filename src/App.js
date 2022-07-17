40077a3623384d6703dd8c59fb4b952d6763160c
Christian <felix.christian.reyes@gmail.com>
2022-07-18 02:29:41 +0800

    added AboutUs component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./pages/Home/Navbar/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import Hero from './pages/Home/Hero/Hero';
+import AboutUs from './pages/Home/AboutUs/AboutUs';
 
 
 
@@ -12,6 +13,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Hero />
+      <AboutUs />
     </div>
   );
 }
