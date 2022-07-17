9a0066bf93dac61ff59b284cbde25186feb8020b
Christian <felix.christian.reyes@gmail.com>
2022-07-18 00:08:33 +0800

    added Hero component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import Navbar from "./pages/Home/Navbar/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import Hero from './pages/Home/Hero/Hero';
+
+
 
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
     </div>
   );
 }
