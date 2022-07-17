53d3946a31dac876b2995425c284a5c37357a6e1
Christian <felix.christian.reyes@gmail.com>
2022-07-18 00:08:23 +0800

    updated tools

---
+++ src/pages/Home/tools.js
@@ -0,0 +1,13 @@
+/**
+   * Easy selector helper function
+   */
+ const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  export default select;
