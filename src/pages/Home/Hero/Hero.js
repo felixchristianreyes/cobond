21a512540faba2e7820905361070c73426ffe984
Christian <felix.christian.reyes@gmail.com>
2022-07-18 01:11:08 +0800

    initial commit

--- src/pages/Home/Hero/Hero.js
+++ src/pages/Home/Hero/Hero.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import './Hero.css';
 
 const Hero = () => (
-  <div className="Hero" data-testid="Hero">
-    Hero Component
-  </div>
+  
+  <section id="hero" class="d-flex align-items-center" data-testid="Hero">
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
+          <h1>Better Solutions For Your Business</h1>
+          <h2>We are team of talented designers making websites with Bootstrap</h2>
+          <div class="d-flex justify-content-center justify-content-lg-start">
+            <a href="#about" class="nav-link btn-get-started scrollto">Get Started</a>
+          </div>
+        </div>
+        <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
+          <img src="assets/img/hero-img.png" class="img-fluid animated" alt="" />
+        </div>
+      </div>
+    </div>
+
+  </section>
 );
 
 Hero.propTypes = {};
