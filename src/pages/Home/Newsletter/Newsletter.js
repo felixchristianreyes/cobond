86bbff3754b1c3fbc64ad24a1331cdfaf8f1bbd7
Christian <felix.christian.reyes@gmail.com>
2022-07-18 21:11:34 +0800

    edited styles and added contents

--- src/pages/Home/Newsletter/Newsletter.js
+++ src/pages/Home/Newsletter/Newsletter.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import './Newsletter.css';
+import React from "react";
+import "./Newsletter.css";
 
 const Newsletter = () => (
-  <div className="Newsletter" data-testid="Newsletter">
-    Newsletter Component
+  <div className="footer-newsletter">
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-lg-6">
+          <h4>Join Our Newsletter</h4>
+          <p>
+            Tamen quem nulla quae legam multos aute sint culpa legam noster
+            magna
+          </p>
+          <form action="" method="post">
+            <input type="email" name="email" />
+            <input type="submit" value="Subscribe" />
+          </form>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
