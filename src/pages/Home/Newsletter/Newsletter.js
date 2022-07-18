861cab858b72cb7d76acecd762a0504228a4838d
Christian <felix.christian.reyes@gmail.com>
2022-07-18 21:05:43 +0800

    initial Newsletter component

---
+++ src/pages/Home/Newsletter/Newsletter.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Newsletter.css';
+
+const Newsletter = () => (
+  <div className="Newsletter" data-testid="Newsletter">
+    Newsletter Component
+  </div>
+);
+
+Newsletter.propTypes = {};
+
+Newsletter.defaultProps = {};
+
+export default Newsletter;
