79df268d4db5444b83e7f5c7c8390141608a0355
Christian <felix.christian.reyes@gmail.com>
2022-07-19 04:22:15 +0800

    added animations

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import './App.css';
+import "./App.css";
 import Navbar from "./pages/Home/Navbar/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Hero from './pages/Home/Hero/Hero';
-import AboutUs from './pages/Home/AboutUs/AboutUs';
-import Services from './pages/Home/Services/Services';
-import Newsletter from './pages/Home/Newsletter/Newsletter';
-import Footer from './pages/Home/Footer/Footer';
+import Hero from "./pages/Home/Hero/Hero";
+import AboutUs from "./pages/Home/AboutUs/AboutUs";
+import Services from "./pages/Home/Services/Services";
+import Newsletter from "./pages/Home/Newsletter/Newsletter";
+import Footer from "./pages/Home/Footer/Footer";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+
 
 function App() {
+  useEffect(() => {
+    Aos.init({
+      duration: 500,
+    });
+  }, []);
   return (
     <div className="App">
+      
       <Navbar />
       <Hero />
       <AboutUs />
