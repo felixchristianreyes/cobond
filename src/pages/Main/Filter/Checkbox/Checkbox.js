88a80b09e7db6c7c67ab44d344c5313adfbee256
Christian <felix.christian.reyes@gmail.com>
2022-07-21 05:08:10 +0800

    created checkbox component

---
+++ src/pages/Main/Filter/Checkbox/Checkbox.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Form from "react-bootstrap/Form";
+
+function Checkbox() {
+  const type = 'checkbox';
+
+  return (
+    <Form className="h-25">
+      
+        <div key={`inline-${type}`} className="mx-3">
+          <Form.Check
+            inline
+            label="Clean"
+            name="group1"
+            type={type}
+            id={`inline-${type}-1`}
+          />
+          <Form.Check
+            inline
+            label="Bearable"
+            name="group1"
+            type={type}
+            id={`inline-${type}-2`}
+          />
+          <Form.Check
+            inline
+            label="Dirty"
+            name="group1"
+            type={type}
+            id={`inline-${type}-3`}
+          />
+        </div>
+    
+    </Form>
+  );
+}
+
+export default Checkbox;
