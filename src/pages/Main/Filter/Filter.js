73813241b006b3c6f274912e843d3a9110ad65a7
Christian <felix.christian.reyes@gmail.com>
2022-07-21 03:48:21 +0800

    base Filter component

---
+++ src/pages/Main/Filter/Filter.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Filter.css';
+
+const Filter = () => (
+  <div className="Filter" data-testid="Filter">
+    Filter Component
+  </div>
+);
+
+Filter.propTypes = {};
+
+Filter.defaultProps = {};
+
+export default Filter;
