21628027c3816d3a52c92c004dcce18739d3cac2
Christian <felix.christian.reyes@gmail.com>
2022-07-21 03:48:53 +0800

    added content and styles

--- src/pages/Main/MainNav/MainNav.js
+++ src/pages/Main/MainNav/MainNav.js
@@ -1,12 +1,52 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './MainNav.css';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import "./MainNav.css";
 
-const MainNav = () => (
-  <div className="MainNav" data-testid="MainNav">
-    
-  </div>
-);
+
+const MainNav = () => {
+
+  return (
+    <>
+      <Navbar
+        sticky="top"
+        bg="dark"
+        variant="dark"
+        expand="lg"
+        datatest-id="MainNav"
+      >
+        <Container>
+          <Navbar.Brand href="#home">GUSBAMPS</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <NavDropdown
+                variant="light"
+                title="Dropdown"
+                id="basic-nav-dropdown"
+              >
+                <NavDropdown.Item href="#action/3.1">
+                  Join Discord
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Contact</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Propose Location
+                </NavDropdown.Item>
+              </NavDropdown>
+
+              <Nav.Link className="loginBtn" href="#login">
+                Login/Register
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
 
 MainNav.propTypes = {};
 
