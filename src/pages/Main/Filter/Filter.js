f0a6a4f50d3ea4cd3a8e1c3ad720ada3d2c45940
Christian <felix.christian.reyes@gmail.com>
2022-07-21 05:08:40 +0800

    added styles and configured for responsivness

--- src/pages/Main/Filter/Filter.js
+++ src/pages/Main/Filter/Filter.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import './Filter.css';
+import React from "react";
+import "./Filter.css";
+import { Container, Card } from "react-bootstrap";
+import StarRating from "./StarRating/StarRating";
+import Checkbox from "./Checkbox/Checkbox";
 
 const Filter = () => (
-  <div className="Filter" data-testid="Filter">
-    Filter Component
-  </div>
+  <Container className="w-50">
+    <h2>Filter By:</h2>
+    <Card className="filterCard">
+      <Container className="d-flex p-2">
+        <p className="p-0 m-0">Ratings:</p> <StarRating />
+      </Container>
+    </Card>
+    <Card className="filterCard">
+      <Container className="d-flex p-2">
+        <p className="p-0 m-0">Cleanliness:</p> <Checkbox />
+      </Container>
+    </Card>
+  </Container>
 );
 
 Filter.propTypes = {};
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -3,11 +3,13 @@ import MainNav from "../Main/MainNav/MainNav";
 import "./Main.css";
 import Footer from "../Home/Footer/Footer";
 import GoogleMapsComponent from "./GoogleMapsComponent/GoogleMapsComponent";
+import Filter from "./Filter/Filter";
 
 const Main = () => (
   <>
     <MainNav />
     <GoogleMapsComponent />
+    <Filter />
     <Footer />
   </>
 );
